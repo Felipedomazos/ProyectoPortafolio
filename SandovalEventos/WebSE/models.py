27f2597f4a08b714e9bacbeb44615df9d29c4ef5
Felipedomazos <fe.domazos@duocuc.cl>
2023-04-10 15:29:17 -0400

    Modelos ejemplos

--- SandovalEventos/WebSE/models.py
+++ SandovalEventos/WebSE/models.py
@@ -31,3 +31,41 @@ class Pedido(models.Model):
         return self.idPedido
 
 
+class DetallePedido(models.Model):
+    cantProducto = models.IntegerField(verbose_name='Cantidad del o los Productos')
+    idPedido = models.ForeignKey(verbose_name='ID del Pedido')
+    idProducto = models.ForeignKey(verbose_name='ID del Producto')
+    rutCliente = models.ForeignKey(max_length=9, verbose_name='RUT del Cliente')
+
+    def __str__(self):
+        return self.idPedido
+
+
+class Producto(models.Model):
+    idProducto = models.UUIDField(primary_key=True, default=uuid.uuid4, verbose_name='ID del Producto')
+    nombreProducto = models.CharField(verbose_name='Nombre del Producto')
+    descProducto = models.CharField(verbose_name='Descripcion del Producto')
+    precioProducto = models.IntegerField(verbose_name='Precio del Producto')
+
+    def __str__(self):
+        return self.idProducto
+
+
+class Evento(models.Model):
+    idEvento = models.IntegerField(primary_key=True, max_length=2, verbose_name='ID del Evento')
+    fechaEvento = models.DateField(verbose_name='Fecha del Evento')
+    cantPersonas = models.IntegerField(verbose_name='Cantidad de Personas del Evento')
+
+    def __str__(self):
+        return self.idEvento
+
+
+class Cotizacion(models.Model):
+    nroCotizacion = models.UUIDField(primary_key=True, default=uuid.uuid4, verbose_name='Numero de la Cotizacion')
+    nombreCotizacion = models.CharField(verbose_name='Nombre de la Cotizacion')
+    rutCliente = models.ForeignKey(max_length=9, verbose_name='RUT del Cliente')
+    idEvento = models.ForeignKey(max_length=2, verbose_name='ID del Evento')
+
+    def __str__(self):
+        return self.nroCotizacion
+
