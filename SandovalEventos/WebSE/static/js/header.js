7fbbc05e3adb2f5b74d0700b204c25610a13c55f
Felipedomazos <fe.domazos@duocuc.cl>
2023-05-05 06:48:19 -0400

    Cambios en el header y agregado de sidebar

---
+++ SandovalEventos/WebSE/static/js/header.js
@@ -0,0 +1,17 @@
+function openSidebar() {
+	document.querySelector('.sidebar').classList.add('sidebar-open');
+  }
+  
+  function closeSidebar() {
+	document.querySelector('.sidebar').classList.remove('sidebar-open');
+  }
+  
+  document.addEventListener('click', function(event) {
+	var sidebar = document.querySelector('.sidebar');
+	if (event.target.closest('.sidebar-container') === null && sidebar.classList.contains('sidebar-open')) {
+	  sidebar.classList.remove('sidebar-open');
+	}
+  });
+
+
+var modal = document.getElementById('id01');
