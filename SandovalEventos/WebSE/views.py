06bf583a2fdb0f80db65522c590512aeba7bdc87
Felipedomazos <fe.domazos@duocuc.cl>
2023-05-07 14:49:30 -0400

    Agregado de Pagina Matrimonio

--- SandovalEventos/WebSE/views.py
+++ SandovalEventos/WebSE/views.py
@@ -6,3 +6,5 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 def Inicio(request):
     return render(request, 'Inicio.html')
+def Matrimonio(request):
+    return render(request, 'Matrimonio.html')
