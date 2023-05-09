cacc75effe0f29d3511eaaa4dc742c2dbe6a7e90
Vina del Mar <vina@duoc.acad>
2023-05-09 16:06:24 -0400

    pagina admin

--- SandovalEventos/WebSE/admin.py
+++ SandovalEventos/WebSE/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Cliente, Pedido, DetallePedido, Producto, Evento, Cotizacion
 
 # Register your models here.
+
+admin.site.register(Cliente)
+admin.site.register(Pedido)
+admin.site.register(DetallePedido)
+admin.site.register(Producto)
+admin.site.register(Evento)
+admin.site.register(Cotizacion)
